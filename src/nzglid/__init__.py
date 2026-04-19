c90aa2d07686ad7f45dd8a8f9aa536cf4d6c829d
Baptiste Hamon <baptiste.hamon@pg.canterbury.ac.nz>
2026-04-19 12:09:12 +1200

    Add __init__ file

---
+++ src/nzglid/__init__.py
@@ -0,0 +1,5 @@
+"""Python Code of the New Zealand Gridded Land Information Dataset (NZGLID)."""
+
+__author__ = "Baptiste Hamon"
+__email__ = "baptiste.hamon@pg.canterbury.ac.nz"
+__version__ = "1.1"
