24980ea976651a198127432e9c37b59a3bb2c503
Baptiste Hamon <baptiste.hamon@pg.canterbury.ac.nz>
2026-04-28 15:05:02 +1200

    Add raster data resampling

--- src/nzglid/resample.py
+++ src/nzglid/resample.py
@@ -1,10 +1,14 @@
 """Resample rasters to 5km and 1km resolution."""
 
 import numpy as np
+from osgeo import gdal
+from pathlib import Path
 import rasterio as rio
 from rasterio.transform import Affine
 from rasterio.warp import reproject, Resampling, calculate_default_transform
 import xarray as xr
+
+from nzglid import DATAPATH, NZGLID_PATH, METADATA, release
 
 def make_exraster(path: str, res: tuple[float | int, float | int]):
     """
@@ -94,3 +98,63 @@ def reproj_match(infile, matchfile, outfile, method='bilinear'):
                     dst_transform=dst_transform,
                     dst_crs=dst_crs,
                     resampling=resampling)
+
+def open_raster(path):
+    """Open raster file as xarray DataArray."""
+    da = xr.open_dataarray(path, engine="rasterio")
+    da = da.rename({"x": "lon", "y": "lat"}).isel(lat=slice(None, None, -1), band=0)
+    return da.drop_vars(["band", "spatial_ref"])
+
+resolutions = {
+    "5km": (0.05, 0.05),
+    "1km": (0.01, 0.01),
+}
+
+for res, grid in resolutions.items():
+
+    if not Path(f"WGS84_{res}.tif").exists():
+        make_exraster(f"WGS84_{res}.tif", grid)
+    
+    res_path = NZGLID_PATH / f"nzglid_{res}"
+    res_path.mkdir(exist_ok=True)
+
+    # Aspect
+    fp = DATAPATH / "lris-nzenvds-nz-environmental-data-stack/nzenvds-aspect-degrees-v10"
+    reproj_match(fp / "nzenvds-aspect-degrees-v10.tif", f"WGS84_{res}.tif", fp / f"aspect_{res}.tif")
+    da = open_raster(fp / f"aspect_{res}.tif").rename("aspect")
+    da.attrs = {
+        **METADATA,
+        'long_name': 'Aspect',
+        'units': 'degrees',
+        'source': 'https://lris.scinfo.org.nz/layer/105706-nzenvds-aspect-degrees-v10/',
+        'comment': 'from field: Band 1'
+    }
+    da.to_netcdf(res_path / f"NZGLID_aspect_{res}_v{release}.nc")
+    
+    # Elevation
+    fp = DATAPATH / "lris-nzdem-25-metre-GTiff"
+    reproj_match(fp / "nzdem.tif", f"WGS84_{res}.tif", fp / f"elevation_{res}.tif")
+    da = open_raster(fp / f"elevation_{res}.tif").rename("elevation")
+    da.attrs = {
+        **METADATA,
+        'long_name': 'Elevation',
+        'units': 'm',
+        'source': 'https://lris.scinfo.org.nz/layer/48131-nzdem-north-island-25-metre/ '
+                  'https://lris.scinfo.org.nz/layer/48127-nzdem-south-island-25-metre/',
+        'comment': 'from field: Band 1'
+    }
+    da.to_netcdf(res_path / f"NZGLID_elevation_{res}_v{release}.nc")
+
+    # Slope
+    fp = DATAPATH / "lris-nzenvds-nz-environmental-data-stack/nzenvds-slope-degrees-v10"
+    reproj_match(fp / "nzenvds-slope-degrees-v10.tif", f"WGS84_{res}.tif", fp / f"slope_{res}.tif")
+    da = open_raster(fp / f"slope_{res}.tif").rename("slope")
+    da = xr.where(da < 0, 0, da)  # correct negative values to 0 (issue likely from resampling)
+    da.attrs = {
+        **METADATA,
+        'long_name': 'Slope',
+        'units': 'degrees',
+        'source': 'https://lris.scinfo.org.nz/layer/107239-nzenvds-slope-degrees-v10/',
+        'comment': 'from field: Band 1'
+    }
+    da.to_netcdf(res_path / f"NZGLID_slope_{res}_v{release}.nc")
