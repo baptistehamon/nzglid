ff25a0a7847caff44db6733db321f0b3048169d9
Baptiste Hamon <baptiste.hamon@pg.canterbury.ac.nz>
2026-04-30 11:13:35 +1200

    Add vector and raster variables

--- src/nzglid/__init__.py
+++ src/nzglid/__init__.py
@@ -24,3 +24,32 @@ RESOLUTION = {
     "5km": (0.05, 0.05),
     "1km": (0.01, 0.01),
 }
+
+VECTOR_VARS = [
+    'cation_exchange_capacity',
+    'drainage',
+    'depth_slowly_permeable_horizon',
+    'erosion_severity',
+    'flood_return_interval',
+    'land_cover',
+    'land_use_capability',
+    'lucas_land_use',
+    'particle_size',
+    'permeability_profile',
+    'ph',
+    'phosphate_retention',
+    'potential_rooting_depth',
+    'profile_readily_available_water',
+    'profile_total_available_water',
+    'rock_outcrops_surface_boulders',
+    'salinity',
+    'soil_temperature_regime',
+    'topsoil_gravel_content',
+    'total_carbon_content',
+]
+
+RASTER_VARS = [
+    "aspect",
+    "elevation",
+    "slope",
+]
